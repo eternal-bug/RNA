566023d47108d93d4c7184a9edad82472dead1ae
song tianqiang <31956395+eternal-bug@users.noreply.github.com>
2019-09-09 22:42:31 +0800

    Update DE_seq.r

--- src/DE_seq.r
+++ src/DE_seq.r
@@ -50,68 +50,72 @@ pheatmap(sampleDistMatrix,
 
 # different sample
 compare_samples <- function(args_list){
-    # args
-    samples = args_list[["samples"]]
-    levels = args_list[["levels"]]
-    dir    = args_list[["dir"]]
-
-    # data sampling
-    coldata_g = subset(coldata, row.names(coldata) %in% samples)
-    countdata_g = countdata[samples]
-    dds = DESeqDataSetFromMatrix(countData = countdata_g, colData = coldata_g, design= ~ condition)
-    dds$condition <- factor(as.vector(dds$condition), levels = levels)
-    dds <- DESeq(dds)
-    # result
-    result <- results(dds, pAdjustMethod = "fdr", alpha = 0.05)
-    result_order <- result[order(result$pvalue),]
-    
-    # print result info and 
-    head(result_order)
-    summary(result_order)
-    table(result_order$padj<0.05)
-
-    # diff gene
-    # the Padj adjusted by FDR, and set threshold value to 0.05
-    # set 1 as threshold value of log2FoldChange and FC is 2/1(^) or 1/2(v)
-    diff_gene <- subset(result_order, padj < 0.05 & abs(log2FoldChange) > 1)
-    summary(diff_gene)
-    # get gene id
-    ensembl_gene_id <- row.names(diff_gene)
-
-    # id transform
-    rat_symbols <- getBM(attributes = c("ensembl_gene_id","external_gene_name","entrezgene_id", "description"),
-                         filters = "ensembl_gene_id", values = ensembl_gene_id, mart = mart)
-
-    # merge symbols to DE analysis data
-    diff_gene$ensembl_gene_id <- ensembl_gene_id
-    # as dataframe
-    diff_gene_dataframe <- as.data.frame(diff_gene)
-    # merge
-    diff_gene_symbols <- merge(diff_gene_dataframe, rat_symbols, by = c("ensembl_gene_id"))
-
-    # write data into file
-    dir.create(dir, recursive = TRUE)
-    write.table(result, paste(dir, "/all_gene.tsv", sep = ""), sep="\t", quote = FALSE)
-    write.table(diff_gene_symbols, paste(dir, "/diff_gene.tsv", sep = ""), row.names = F,sep="\t", quote = FALSE)
-
-    # enrichment analysis
-    for(i in c("CC", "BP", "MF")){
-        ego <- enrichGO(gene          = rat_symbols$ensembl_gene_id,
-                        OrgDb         = org.Rn.eg.db,
-                        keyType       = 'ENSEMBL',
-                        ont           = i,
-                        pAdjustMethod = "BH",
-                        pvalueCutoff  = 0.01,
-                        qvalueCutoff  = 0.05)
-        pdf(paste(dir, "/", i, ".pdf", sep = ""))
-            dotplot(ego, showCategory = 30, title = paste("The GO ", i, " enrichment analysis", sep = ""))
-        dev.off()
-    }
+  # args
+  samples = args_list[["samples"]]
+  levels = args_list[["levels"]]
+  dir    = args_list[["dir"]]
+  
+  # data sampling
+  coldata_g = subset(coldata, row.names(coldata) %in% samples)
+  countdata_g = countdata[samples]
+  dds = DESeqDataSetFromMatrix(countData = countdata_g, colData = coldata_g, design= ~ condition)
+  dds$condition <- factor(as.vector(dds$condition), levels = levels)
+  dds <- DESeq(dds)
+  # result
+  result <- results(dds, pAdjustMethod = "fdr", alpha = 0.05)
+  result_order <- result[order(result$pvalue),]
+  
+  # print result info and 
+  print("====> the order result summary.")
+  head(result_order)
+  summary(result_order)
+  table(result_order$padj<0.05)
+  
+  # diff gene
+  # the Padj adjusted by FDR, and set threshold value to 0.05
+  # set 1 as threshold value of log2FoldChange and FC is 2/1(^) or 1/2(v)
+  print("====> the different genes summary.")
+  diff_gene <- subset(result_order, padj < 0.05 & abs(log2FoldChange) > 1)
+  summary(diff_gene)
+  # get gene id
+  ensembl_gene_id <- row.names(diff_gene)
+  
+  # id transform
+  rat_symbols <- getBM(attributes=c("ensembl_gene_id","external_gene_name","entrezgene_id", "description"),
+                       filters = 'ensembl_gene_id', values = ensembl_gene_id, mart = mart)
+  
+  # merge symbols to DE analysis data
+  diff_gene$ensembl_gene_id <- ensembl_gene_id
+  # as dataframe
+  diff_gene_dataframe <- as.data.frame(diff_gene)
+  # merge
+  diff_gene_symbols <- merge(diff_gene_dataframe, rat_symbols, by = c("ensembl_gene_id"))
+  
+  # write data into file
+  print("====> write the table.")
+  dir.create(dir, recursive = TRUE)
+  write.table(result, paste(dir, "/all_gene.tsv", sep = ""), sep="\t", quote = FALSE)
+  write.table(diff_gene_symbols, paste(dir, "/diff_gene.tsv", sep = ""), row.names = F,sep="\t", quote = FALSE)
+  
+  # enrichment analysis
+  for(i in c("CC", "BP", "MF")){
+    print(paste("====> begin to GO ", i, " analysis", sep = ""))
+    ego <- enrichGO(gene          = rat_symbols$ensembl_gene_id,
+                    OrgDb         = org.Rn.eg.db,
+                    keyType       = "ENSEMBL",
+                    ont           = paste(i, sep = ""),
+                    pAdjustMethod = "BH",
+                    pvalueCutoff  = 0.01,
+                    qvalueCutoff  = 0.05)
+    pdf(paste(dir, "/", i, ".pdf", sep = ""))
+        dotplot(ego, showCategory = 30, title = paste("The GO ", i, " enrichment analysis", sep = ""))
+    dev.off()
+  }
 }
 
-compare_samples( list(samples=c("NH", "LHA1", "LHA2", "LHA3"), levels = c("NC","H1"), dir = "../stat/HA") )
-compare_samples( list(samples=c("NH", "LHB1", "LHB2"        ), levels = c("NC","H2"), dir = "../stat/HB") )
-compare_samples( list(samples=c("NH", "LHC1", "LHC2", "LHC3"), levels = c("NC","H3"), dir = "../stat/HC") )
+compare_samples( list(samples=c("NH", "LHA1", "LHA2", "LHA3"), levels = c("HC","H1"), dir = "../stat/HA") )
+compare_samples( list(samples=c("NH", "LHB1", "LHB2"        ), levels = c("HC","H2"), dir = "../stat/HB") )
+compare_samples( list(samples=c("NH", "LHC1", "LHC2", "LHC3"), levels = c("HC","H3"), dir = "../stat/HC") )
 compare_samples( list(samples=c("NM", "LMA1", "LMA2", "LMA3"), levels = c("MC","M1"), dir = "../stat/MA") )
 compare_samples( list(samples=c("NH", "LMB1", "LMB2", "LMB3"), levels = c("MC","M2"), dir = "../stat/MB") )
 compare_samples( list(samples=c("NH", "LMC1", "LHC2"        ), levels = c("MC","M3"), dir = "../stat/MC") )
